eba5eb528aaeddd07b949540d0bc6a1d10212b75
Bushra Ankhari <bushra.ankhari@welldev.iocleargit config --global user.email bushra.ankhari@welldev.io>
2023-04-04 16:07:15 +0600

    markdone changes after first review

--- js/markDone.js
+++ js/markDone.js
@@ -1,18 +1,17 @@
-import { LIST } from "./addToTask.js"; //named import
-import { list } from "./constants.js"; //named import
+import { LIST } from "/js/addToTask.js";
+import { list, CLICK_EVENT, EDIT, COMPLETE } from "/js/constants.js";
 
 function completeToDo(element) {
+  LIST[element.id].done = true;
   element.parentNode.querySelector(".text").style.textDecoration =
     "line-through";
   element.parentNode.querySelector(".text").style.color = "green";
   element.style.display = "none";
-  element.parentNode.querySelector("[job='edit']").style.display = "none";
+  element.parentNode.querySelector(`[job="${EDIT}"]`).style.display = "none";
   const taskStartday = element.parentNode.querySelector(".date").innerText;
-  console.log(taskStartday);
-  var start = taskStartday.replace(/[^0-9]/g, "-");
+  let start = taskStartday.replace(/[^0-9]/g, "-");
   start = start.slice(12);
-  console.log("start ", start);
-  var startdate = new Date(start);
+  const startdate = new Date(start);
 
   const enddate = new Date();
   let duration = parseInt((enddate - startdate) / 86400000);
@@ -24,15 +23,12 @@ function completeToDo(element) {
   el.classList.add("duration");
   el.textContent = `Completed in ${duration} days`;
   element.parentNode.appendChild(el);
-  LIST[element.id].done = true;
 }
 
-list.addEventListener("click", function (event) {
+list.addEventListener(CLICK_EVENT, function (event) {
   const element = event.target;
   const elementJob = element.attributes.job.value;
-  if (elementJob == "complete") {
+  if (elementJob == COMPLETE) {
     completeToDo(element);
   }
-  //add item to localstorage
-  localStorage.setItem("TODO", JSON.stringify(LIST));
 });
