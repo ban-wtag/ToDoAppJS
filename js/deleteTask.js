71c6b7fe15346b91eece55e518730f8acea54717
Bushra Ankhari <bushra.ankhari@welldev.iocleargit config --global user.email bushra.ankhari@welldev.io>
2023-04-06 12:54:25 +0600

    after 3rd

--- js/deleteTask.js
+++ js/deleteTask.js
@@ -1,10 +1,10 @@
-import { LIST } from "/js/addToTask.js";
-import { list, CLICK_EVENT, DELET } from "/js/constants.js";
+import { taskLIST } from "/js/addToTask.js";
+import { UL_LIST, CLICK_EVENT, DELET } from "/js/constants.js";
 
 function removeToDos(element) {
   element.parentNode.parentNode.removeChild(element.parentNode);
-  LIST[element.id].trash = true;
-  LIST.splice(element.id, 1);
+  taskLIST[element.id].trash = true;
+  taskLIST.splice(element.id, 1);
 }
 
 list.addEventListener(CLICK_EVENT, function (event) {
