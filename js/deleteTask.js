207bf4cd4c8efdfa6cdf9522ccd0195b43a55d42
ban-wtag <bushra.ankhari@welldev.io>
2023-04-08 13:21:14 +0600

    removing splice and adding filter

--- js/deleteTask.js
+++ js/deleteTask.js
@@ -1,19 +1,21 @@
-import { taskLIST } from "/js/addToTask.js";
+//import { taskList } from "/js/addToTask.js";
 import { CLICK_EVENT, DELET, UL_LIST } from "/js/constants.js";
-
-function removeToDos(element) {
-  let index = taskLIST.findIndex((item) => {
+taskList;
+function removeTodos(element) {
+  let index = taskList.findIndex((item) => {
     return item.id == element.id;
   });
-  taskLIST[index].trash = true;
+  taskList[index].trash = true;
   element.parentNode.parentNode.removeChild(element.parentNode);
-  taskLIST.splice(index, 1);
+
+const afterRemovalList = taskList.filter(item => item.id !== element.id);
+taskList = afterRemovalList;
 }
 
 UL_LIST.addEventListener(CLICK_EVENT, function (event) {
   const element = event.target;
   const elementJob = element.attributes.job.value;
   if (elementJob == DELET) {
-    removeToDos(element);
+    removeTodos(element);
   }
 });
