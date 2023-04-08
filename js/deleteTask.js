45908cb8ed4e7dccb1161ae74c0eedb04cdcd2f8
ban-wtag <bushra.ankhari@welldev.io>
2023-04-08 14:05:11 +0600

    using filter

--- js/deleteTask.js
+++ js/deleteTask.js
@@ -1,15 +1,14 @@
 //import { taskList } from "/js/addToTask.js";
 import { CLICK_EVENT, DELET, UL_LIST } from "/js/constants.js";
-taskList;
+
 function removeTodos(element) {
   let index = taskList.findIndex((item) => {
     return item.id == element.id;
   });
   taskList[index].trash = true;
   element.parentNode.parentNode.removeChild(element.parentNode);
-
-const afterRemovalList = taskList.filter(item => item.id !== element.id);
-taskList = afterRemovalList;
+  const afterRemovalList = taskList.filter((item) => item.id !== element.id);
+  taskList = afterRemovalList;
 }
 
 UL_LIST.addEventListener(CLICK_EVENT, function (event) {
