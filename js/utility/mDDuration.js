697a89dee5e3f95201423463c1360a27d91beb42
Bushra Ankhari <bushra.ankhari@welldev.iocleargit config --global user.email bushra.ankhari@welldev.io>
2023-04-17 13:30:07 +0600

    step-4 handling corner cases

--- js/utility/mDDuration.js
+++ js/utility/mDDuration.js
@@ -1,6 +1,5 @@
-export default function calculateDuration(startdate) {
-  const enddate = new Date().getTime();
-  let duration = parseInt((enddate - startdate) / 86400000);
-  duration += 1;
+export default function calculateDuration(startDate) {
+  const endDate = new Date().getTime();
+  let duration = parseInt((endDate - startDate) / 86400000) + 1;
   return duration;
 }
