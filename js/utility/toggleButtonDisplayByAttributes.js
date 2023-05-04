eae0036e7c00ff01988042ae5240ac5db316d60b
Bushra Ankhari <bushra.ankhari@welldev.iocleargit config --global user.email bushra.ankhari@welldev.io>
2023-05-04 13:27:20 +0600

    testing consoles removed

--- js/utility/toggleButtonDisplayByAttributes.js
+++ js/utility/toggleButtonDisplayByAttributes.js
@@ -1,5 +1,4 @@
 export default function toggleButtonDisplayByAttributes(element, ...attrs) {
-  console.log("k0 inside toggle");
   attrs.forEach((attr) => {
     if (element.querySelector(attr).style.display === "none") {
       element.querySelector(attr).style.display = "inline-block";
